0bfc9bb24dde7a7684f8c26238e82d468b6b92d4
Yuki Nagano <newsnsd.24@gmail.com>
2022-02-19 18:34:58 -0800

    fix: change STATIC_URL

--- kintaidemo/settings.py
+++ kintaidemo/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'kintaiapp/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
