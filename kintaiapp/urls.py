c9a6cdd1c3a5f9e9d0c6507fffa70247aa29bc90
Yuki Nagano <newsnsd.24@gmail.com>
2022-02-19 17:53:00 -0800

    feat: change endpoint

--- kintaiapp/urls.py
+++ kintaiapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dokintai', views.dokintai),
+    path('', views.dokintai),
     # test
     path('hello', views.index),
 ]
