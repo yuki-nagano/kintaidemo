3200ef786554b70d275faed27e623c7c8e051f96
Yuki Nagano <newsnsd.24@gmail.com>
2022-02-25 13:37:33 -0800

    feat: add kintaiAdmin - field and search

--- kintaiapp/admin.py
+++ kintaiapp/admin.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.contrib import admin
+from .models import Kintai
 
 # Register your models here.
+class KintaiAdmin(admin.ModelAdmin):
+    model = Kintai
+    list_display = ('u_id', 'workingday', 'begintime', 'finishtime', 'breaktime')
+    search_fields = ('u_id',)
+
+admin.site.register(Kintai, KintaiAdmin)
