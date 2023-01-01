2343b7e8da9d49c701a6ab47c1598b99d783183e
Yuki Nagano <newsnsd.24@gmail.com>
2023-01-01 15:52:31 -0800

    reverse some urls path

--- kintaiapp/urls.py
+++ kintaiapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dokintai', views.dokintai, name='dokintai'),
-    path('record', views.RecordViews, name='record'),
-    path('record/monthly', views.RecordViews, name='record/monthly'),
+    path('record', views.record, name='record'),
+    path('record/monthly', views.record, name='record/monthly'),
     path('export/csv', views.export_csv, name='export_csv'),
 ]
