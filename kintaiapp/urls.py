7d66789c47b2ca61ed038efc4c55d662c628f355
Yuki Nagano <newsnsd.24@gmail.com>
2023-01-03 23:50:25 -0800

    change to class-based views

--- kintaiapp/urls.py
+++ kintaiapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import RecordViews
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('dokintai', views.dokintai, name='dokintai'),
-    path('record', views.record, name='record'),
-    path('record/monthly', views.record, name='record/monthly'),
+    path('record', RecordViews.as_view(), name='record'),
+    path('record/monthly', RecordViews.as_view(), name='record/monthly'),
     path('export/csv', views.export_csv, name='export_csv'),
 ]
--- kintaiapp/views.py
+++ kintaiapp/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 from kintaiapp.models import Kintai, WorkingStatus
 from datetime import datetime, timedelta
@@ -142,57 +143,9 @@ def dokintai(request):
 
 
 ###
-# RECORD
-#  - 勤怠一覧表示
-#  - 勤怠編集 (追加予定)
-###
-def record(request):
-    # 一覧表示
-    if request.method == 'GET':
-        # Get values from query params
-        if 'month' in request.GET and 'year' in request.GET:
-            year = int(request.GET['year'])
-            month = int(request.GET['month'])
-        else:
-            # if there's no query params, show this month
-            year = datetime.today().year
-            month = datetime.today().month
-
-        data_dict = _get_record_by_month(year, month)
-        return render(request, 'kintaiapp/record.html', data_dict)
-
-
-###
-# Get record by month
-# 月毎の勤怠を取得
-# param:
-#   year : int
-#   month: int
-###
-def _get_record_by_month(year, month):
-    date = datetime(year, month, 1)
-    data = Kintai.objects.filter(begintime__year=year, begintime__month=month).order_by('id')  # memo: order_by 降順は'-id'
-    last_month = date - relativedelta(months=1)
-    next_month = date + relativedelta(months=1)
-    data_dict = {
-        'kintailist': data,
-        'datelist': [
-            {
-                'lastmonth_year': last_month.year,
-                'lastmonth_month': last_month.month,
-                'nextmonth_year': next_month.year,
-                'nextmonth_month': next_month.month,
-            }
-        ],
-    }
-
-    return data_dict
-
-
-###
 # Calculate break time with start time and end time
 # 開始時間と終了時間から休憩時間を計算
-# 
+#
 # 現在の設定
 # 　 - 4時間勤務：30分休憩
 # 　 - 8時間勤務：1時間休憩
@@ -208,3 +161,52 @@ def _calc_breaktime(start, end):
     elif total_hour.seconds >= 28800:
         # 8時間以上
         return timedelta(seconds=3600)
+
+###
+# RECORD VIEWS
+#  - 勤怠一覧表示
+#  - 勤怠編集 (追加予定)
+###
+class RecordViews(View):
+
+    def get(self, request):
+        # Get values from query params
+        if 'month' in request.GET and 'year' in request.GET:
+            year = int(request.GET['year'])
+            month = int(request.GET['month'])
+        else:
+            # if there's no query params, show this month
+            year = datetime.today().year
+            month = datetime.today().month
+
+        data_dict = self._get_record_by_month(year, month)
+        return render(request, 'kintaiapp/record.html', data_dict)
+
+
+    ###
+    # Get record by month
+    # 月毎の勤怠を取得
+    # param:
+    #   year : int
+    #   month: int
+    ###
+    def _get_record_by_month(self, year, month):
+        date = datetime(year, month, 1)
+        data = Kintai.objects.filter(begintime__year=year, begintime__month=month).order_by(
+            'id')  # memo: order_by 降順は'-id'
+        last_month = date - relativedelta(months=1)
+        next_month = date + relativedelta(months=1)
+        data_dict = {
+            'kintailist': data,
+            'datelist': [
+                {
+                    'lastmonth_year': last_month.year,
+                    'lastmonth_month': last_month.month,
+                    'nextmonth_year': next_month.year,
+                    'nextmonth_month': next_month.month,
+                }
+            ],
+        }
+
+        return data_dict
+
