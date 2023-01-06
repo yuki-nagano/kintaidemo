fb579336d3e4928d390282e3bbc90966a2bc413d
Yuki Nagano <newsnsd.24@gmail.com>
2023-01-05 23:45:26 -0800

    add edit record

--- kintaiapp/urls.py
+++ kintaiapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import RecordViews
+from .views import RecordViews, RecordUpdateViews
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('record', RecordViews.as_view(), name='record'),
     path('record/monthly', RecordViews.as_view(), name='record/monthly'),
     path('export/csv', views.export_csv, name='export_csv'),
+    path('record/edit/<int:pk>', RecordUpdateViews.as_view(), name='record/edit'),
 ]
