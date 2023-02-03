fd24e048bcf881c8e68dcc6fba85a13624adc125
Yuki Nagano <newsnsd.24@gmail.com>
2023-02-02 23:21:02 -0800

    modify database settings

--- kintaidemo/settings.py
+++ kintaidemo/settings.py
@@ -95,17 +95,28 @@ WSGI_APPLICATION = 'kintaidemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME', 'kintaidemo'),
-        'USER': os.getenv('USER', 'kintaidemo'),
-        'PASSWORD': os.getenv('PASSWORD', 'test'),
-        'HOST': os.getenv('HOST', 'docker.for.mac.localhost'),
-        'PORT': os.getenv('PORT', '5432'),
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'kintaidemo',
+            'USER': 'kintaidemo',
+            'PASSWORD': 'test',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('NAME'),
+            'USER': os.getenv('USER'),
+            'PASSWORD': os.getenv('PASSWORD'),
+            'HOST': os.getenv('HOST'),
+            'PORT': os.getenv('PORT'),
+        }
+    }
 
 
 # Password validation
