dade38cc93b536db6ead7b3aa8c1062ee3869c2f
Yuki Nagano <newsnsd.24@gmail.com>
2023-02-02 22:11:41 -0800

    modify db config and set default

--- kintaidemo/settings.py
+++ kintaidemo/settings.py
@@ -29,7 +29,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', False)
 
 # for local, will set it up by host later
 # DEBUG = True
