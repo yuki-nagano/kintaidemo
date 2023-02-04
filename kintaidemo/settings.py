a6b0843ad2023e6df25ab9572f1a2dcf255d96f0
Yuki Nagano <newsnsd.24@gmail.com>
2023-02-04 15:25:22 -0800

    revert STATICFILES_DIRS

--- kintaidemo/settings.py
+++ kintaidemo/settings.py
@@ -157,9 +157,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'kintaiapp/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'kintaiapp/static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'kintaiapp/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
